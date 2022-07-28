8b6687bccdb43d5d7e11fed665f66a70bd616199
Dennis Klein <d.klein@gsi.de>
2022-07-29 01:13:09 +0200

    feat: Implement `id` and `systems` commands

--- fnt/__init__.py
+++ fnt/__init__.py
@@ -1,3 +1,43 @@
 """REST client for FNT Command (https://www.fntsoftware.com/en/products/fnt-command)"""
 
+import requests
+
 VERSION = '0.0.2'
+
+
+class UnsuccessfulStatusResponse(Exception):
+    """json response body contains status.success == False"""
+    def __init__(self, status):
+        self.message = status['message']
+        self.error_code = status['errorCode']
+        self.sub_error_code = status['subErrorCode']
+
+    def __str__(self):
+        return f'{self.message} ({self.error_code}, {self.sub_error_code})'
+
+
+class RESTClient:
+    def __init__(self, **kwargs):
+        self.config = kwargs
+        self.base_url = self.config['server']
+        self.base_path = '/axis/api/rest'
+
+    def id(self):
+        """Query business gateway ID"""
+        path = f'{self.base_path}/businessGateway/Id'
+        req = requests.get(f'{self.base_url}{path}')
+        req.raise_for_status()
+        res = req.json()
+        if not res['status']['success']:
+            raise UnsuccessfulStatusResponse(res['status'])
+        return res['identifier']
+
+    def systems(self):
+        """Query business gateway systems"""
+        path = f'{self.base_path}/businessGateway/Systems'
+        req = requests.get(f'{self.base_url}{path}')
+        req.raise_for_status()
+        res = req.json()
+        if not res['status']['success']:
+            raise UnsuccessfulStatusResponse(res['status'])
+        return res['systems']
--- fnt/cli.py
+++ fnt/cli.py
@@ -1,2 +1,73 @@
-def main():
-    print('Hello world!')
+from array import array
+import click
+from fnt import RESTClient
+import os
+from pathlib import Path
+from rich.console import Console
+from rich.table import Table
+import tomli
+
+
+@click.group()
+@click.option('-c',
+              '--connection',
+              default=None,
+              help='Connection name from config.toml.')
+@click.pass_context
+def main(ctx, connection):
+    ctx.ensure_object(dict)
+    ctx.obj['connection'] = connection
+    return 0
+
+
+def make_restclient(connection):
+    config_home = Path(os.getenv('XDG_CONFIG_HOME', None))
+    if not config_home:
+        config_home = Path(os.getenv('HOME'), None) / '.config'
+
+    with open(config_home / 'fnt/config.toml', 'rb') as f:
+        conf = tomli.load(f)
+        if not connection:
+            connection = conf['default_connection']
+        return RESTClient(**conf['connection'][connection])
+
+
+@main.command()
+@click.pass_context
+def id(ctx):
+    """Print business gateway ID."""
+    try:
+        connection = ctx.obj['connection']
+        fnt = make_restclient(connection)
+        click.echo(fnt.id())
+        return 0
+    except Exception:
+        Console().print_exception(suppress=[click])
+        return 1
+
+
+@main.command()
+@click.pass_context
+def systems(ctx):
+    """Print business gateway systems."""
+    try:
+        connection = ctx.obj['connection']
+        fnt = make_restclient(connection)
+        systems = fnt.systems()
+
+        table = Table()
+        table.add_column('id')
+        table.add_column('system')
+        table.add_column('name')
+        table.add_column('version')
+        for system in systems:
+            table.add_row(*[
+                system['id'], system['system'], system['name'],
+                system['version']
+            ])
+
+        Console().print(table)
+        return 0
+    except Exception:
+        Console().print_exception(suppress=[click])
+        return 1
