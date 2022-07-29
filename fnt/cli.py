0e656cc95d4f7ffa6123206624f3d9af9a9d0694
Dennis Klein <d.klein@gsi.de>
2022-07-29 15:05:11 +0200

    feat: Replace `id` and `systems` with `sysinfo`

--- fnt/cli.py
+++ fnt/cli.py
@@ -5,21 +5,28 @@ from pathlib import Path
 from rich.console import Console
 from rich.table import Table
 import tomli
+import typing as t
+from click.core import Context
+from functools import wraps
+
+F = t.TypeVar("F", bound=t.Callable[..., t.Any])
 
 
-@click.group()
-@click.option('-c',
-              '--connection',
-              default=None,
-              help='Connection name from config.toml.')
-@click.pass_context
-def main(ctx, connection) -> int:
-    ctx.ensure_object(dict)
-    ctx.obj['connection'] = connection
-    return 0
+def pp_exceptions(f: F) -> F:
+    """Decorator that catches all exceptions and pretty prints it"""
+    @wraps(f)
+    def wrapper(*args, **kwds):  # type: ignore
+        try:
+            return f(*args, **kwds)
+        except Exception:
+            Console().print_exception(suppress=[click])
+            return 1
+
+    return wrapper
 
 
 def make_client(connection: str) -> Client:
+    """Factory for an fnt.Client"""
     config_home = Path(str(os.getenv('XDG_CONFIG_HOME')))
     if not config_home:
         config_home = Path(str(os.getenv('HOME'))) / '.config'
@@ -31,42 +38,45 @@ def make_client(connection: str) -> Client:
         return Client(**conf['connection'][connection])
 
 
-@main.command()
-@click.pass_obj
-def gateway_id(obj: dict) -> int:
-    """Print business gateway ID."""
-    try:
-        connection = obj['connection']
-        fnt = make_client(connection)
-        click.echo(fnt.id())
-        return 0
-    except Exception:
-        Console().print_exception(suppress=[click])
-        return 1
+def pass_client(f: F) -> F:
+    """Decorator that creates and passes a fnt.Client"""
+    @wraps(f)
+    @click.pass_obj
+    def wrapper(obj: dict, *args, **kwds):  # type: ignore
+        client = make_client(obj['connection'])
+        return f(client, *args, **kwds)
+
+    return wrapper
+
+
+@click.group()
+@click.option('-c',
+              '--connection',
+              default=None,
+              help='Connection name from config.toml.')
+@click.pass_context
+def main(ctx: Context, connection: str) -> int:
+    ctx.ensure_object(dict)
+    ctx.obj['connection'] = connection
+    return 0
 
 
 @main.command()
-@click.pass_obj
-def systems(obj: dict) -> int:
-    """Print business gateway systems."""
-    try:
-        connection = obj['connection']
-        fnt = make_client(connection)
-        systems = fnt.systems()
+@pass_client
+@pp_exceptions
+def sysinfo(fnt: Client) -> int:
+    """Print business gateway id and systems."""
+    gwid = fnt.id()
+    systems = fnt.systems()
 
-        table = Table()
-        table.add_column('id')
-        table.add_column('system')
-        table.add_column('name')
-        table.add_column('version')
-        for system in systems:
-            table.add_row(*[
-                system['id'], system['system'], system['name'],
-                system['version']
-            ])
+    cols = ('id', 'system', 'name', 'version')
+    table = Table()
+    for col in cols:
+        table.add_column(col)
+    for system in systems:
+        table.add_row(*[system[col] for col in cols])
 
-        Console().print(table)
-        return 0
-    except Exception:
-        Console().print_exception(suppress=[click])
-        return 1
+    console = Console()
+    console.print(f'Business Gateway ID: {gwid}')
+    console.print(table)
+    return 0
